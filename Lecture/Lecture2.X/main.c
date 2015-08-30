5ab7cf241aaa2f54f68db6572677a38414570cb8
hitpotato <clongkb@gmail.com>
2015-08-29 18:55:47 -0700

    just main.c

---
+++ Lecture/Lecture2.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: longchen
+ *
+ * Created on August 29, 2015, 6:47 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
